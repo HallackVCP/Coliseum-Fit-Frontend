ec62db8c7ee2ca85b0f4f39073f3104d2fa74060
Pedro Couto <pedrocouto152001@gmail.com>
2022-07-05 11:25:35 -0300

    Correção das cores

--- lib/shared/themes/app_colors.dart
+++ lib/shared/themes/app_colors.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
 class AppColors{
-  static Color background = Color(0xFF062029);
-  static Color primary = Color(0xFFD0FD3E);
-  static Color backgroundButton = Color(0xFFC4C4C4).withOpacity(0.2);
+  static const Color background = Color(0xFF062029);
+  static const Color primary = Color(0xFFD0FD3E);
+  static const Color seaGreen = Color(0xFF008148);
+  static const Color warningtangerine = Color(0xFFEF8A17);
+  static const Color warningRed = Color(0xFFEF2917);
+  static const Color border = Colors.grey;
+  static const Color backgroundButton = Color(0xFFC4C4C4);
 }
 
 const MaterialColor MaterialCustomColor = MaterialColor(
     0xFF276678,
     <int, Color> {
-      50: Color(0xFFF6F5F5),
-      100: Color(0xFFD3E0EA),
-      200: Color(0xFFD3E0EA),
-      300: Color(0xFFD3E0EA),
-      400: Color(0xFF1687A7),
-      500: Color(0xFF276678),
-      600: Color(0xFFD3E0EA),
-      700: Color(0xFF1687A7),
+      50: AppColors.background,
+      100: AppColors.primary,
+      200: AppColors.seaGreen,
+      300: AppColors.warningtangerine,
+      400: AppColors.warningRed,
+      500: AppColors.border,
+      600: AppColors.backgroundButton,
+      700: AppColors.background,
     }
   );
